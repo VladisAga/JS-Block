ddaaff3cfc70c13f7dba9a145269c2acdafd52f5
VladisAga <boss7551va@gmail.com>
2023-06-11 02:30:43 +0300

    hm3

---
+++ hm4/index.js
@@ -0,0 +1,68 @@
+/*
+let aboba = {
+    firstBoba: 'boba',
+    secondBoba: 'biba',
+}
+
+delete aboba.firstBoba;
+delete aboba.secondBoba;
+*/
+
+/*
+let aboba = {
+    firstBoba: 'boba',
+    secondBoba: 'biba',
+};
+
+if ('firstBoba' in aboba) {
+    console.log(true);
+};
+*/
+
+/*
+const student = {
+    name: 'John',
+    age: 19,
+    isHappy: true
+};
+
+for(let key in student) {
+console.log(key);
+console.log(student[key]);
+};
+*/
+
+/*
+const colors = {
+    'ru pum pu ru rum': {
+        red: 'красный',
+        green: 'зеленый',
+        blue: 'синий'
+    },
+};
+
+console.log(colors['ru pum pu ru rum']['red']);
+console.log(colors['ru pum pu ru rum']['blue']);
+*/
+
+/*
+let salaries = {
+    andrey: 500,
+    sveta: 413,
+    anton: 987,
+    andrey2: 664,
+    alexandra: 199
+};
+
+let objLength = Object.keys(salaries).length;
+let averageSal = 0;
+
+for (let key in salaries) {
+    averageSal += salaries[key];
+}
+
+averageSal = averageSal / objLength;
+
+console.log(averageSal);
+*/
+
